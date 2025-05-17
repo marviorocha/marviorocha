a42a416de14cbe95eb05f2ede56b135876b803f2
Marvio Rocha <marviorocha@marviorocha>
2025-05-16 22:23:06 -0300

    Update for deploy

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-// import { defineMiddleware } from 'astro/middleware';
-
-// export const onRequest = defineMiddleware(({ request, redirect }) => {
-//     const lang = request.headers.get("accept-language")?.startsWith("pt") ? "pt" : "en";
-//     return redirect(`/${lang}/`);
-// });
